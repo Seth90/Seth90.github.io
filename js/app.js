46cccec6107a500698d59156423c433eb6851727
Seth90 <seth_90@mail.ru>
2023-05-24 01:08:46 +0300

    2023-05-24

--- js/app.js
+++ js/app.js
@@ -1,14 +1,54 @@
 const anchors = document.querySelectorAll('a[href*="#"]')
 
-        for (let anchor of anchors) {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault()
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        const blockID = anchor.getAttribute('href').substr(1)
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
 
-                const blockID = anchor.getAttribute('href').substr(1)
-
-                document.getElementById(blockID).scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                })
-            })
+let AgeCounter = (el) => {
+    //console.log('agecounter');
+    let counts = setInterval(updated, 50);
+    let upto = 0;
+    function updated() {
+        el.innerHTML = upto < 9 ? '0' + ++upto : ++upto;
+        if (upto === 33) {
+            clearInterval(counts);
         }
+    }
+}
+
+var element = document.querySelector('#age');
+var done = false;
+var Visible = function (target) {
+    var targetPosition = {
+        top: window.pageYOffset + target.getBoundingClientRect().top,
+        bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+    };
+    windowPosition = {
+        top: window.pageYOffset,
+        bottom: window.pageYOffset + document.documentElement.clientHeight
+    };
+    console.log(targetPosition.bottom, windowPosition.top)
+    if (targetPosition.bottom > windowPosition.top &&
+        targetPosition.top < windowPosition.bottom) {
+        console.log('visible2');
+        if (!done) {
+            AgeCounter(element); done = true;
+        }
+    } else {
+        element.innerHTML = 0;
+        done = false;
+    };
+};
+
+window.addEventListener('scroll', function () {
+    Visible(element);
+});
+
+Visible(element);
