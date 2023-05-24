5ca250d6e118f6442bdb21bfa3ad4f9859ce44ec
Seth90 <seth_90@mail.ru>
2023-05-24 23:28:47 +0300

    2023-05-24

--- js/app.js
+++ js/app.js
@@ -56,21 +56,43 @@ Visible(element);
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: true,
-  
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 5
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 5
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 10
+        }
+    },
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     // scrollbar: {
     //   el: '.swiper-scrollbar',
     // },
-  });
+});
